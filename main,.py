5c4cb2330d73ece9afee516e86565adbaf8d8366
Tucupitox <paericmanuel@gmail.com>
2026-05-16 11:42:47 -0400

    Create main.py file and README

---
+++ main,.py
@@ -0,0 +1,102 @@
+import pygame
+from random import *
+
+pygame.init()
+
+ANCHO, ALTO = 500, 500
+COLOR_FONDO = (64, 201, 144)
+COLOR_CARD = (227, 172, 54)
+COLOR_BORDER = (71, 11, 222)
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+GREEN = (25, 235, 14)
+RED = (235, 14, 29)
+FPS = 40
+
+screen = pygame.display.set_mode((ANCHO, ALTO))
+reloj = pygame.time.Clock()
+
+# CLASES 
+class Area():
+    def __init__(self, x, y, ancho, alto, color=None):
+        self.rect = pygame.Rect(x, y, ancho, alto)
+        self.color = color
+    
+    def fill(self):
+        pygame.draw.rect(screen, self.color, self.rect)
+
+    def change_color(self, new_color):
+        self.color = new_color
+
+    def set_border(self, border_color, border_size):
+        pygame.draw.rect(screen, border_color, self.rect, border_size)
+    
+    def is_collide(self, x, y):
+        # devuelve True o False en caso de colision con el rectangulo
+        return self.rect.collidepoint(x, y) 
+
+class Label(Area):
+    def set_text(self, text, size, text_color=BLACK):
+        self.image = pygame.font.SysFont("Arial", size).render(text, 1, text_color)
+
+    def draw(self, dist_x=10, dist_y=10):
+        self.fill()
+        screen.blit(self.image, (self.rect.x + dist_x, self.rect.y + dist_y))
+
+# CREANDO OBJETOS
+lista_cards = []
+x = 50
+for i in range(4):
+    card = Label(x, 150, 80, 100, COLOR_CARD)
+    card.set_text('Click!', 24)
+    lista_cards.append(card)
+    x += 110
+
+# TEXTO PARA ESTADISTICAS
+point_text = Label(50, 50, 200, 50, COLOR_FONDO)
+
+wait = 0
+points = 0
+
+while True:
+    if wait == 0:
+        screen.fill(COLOR_FONDO)
+        point_text.set_text(f'PUNTAJE: {points}', 30)
+        point_text.draw(10, 10)
+
+        wait = 20
+        click = randint(0, 3)
+
+        for i in range(4):
+            lista_cards[i].change_color(COLOR_CARD)
+            if i == click:
+                lista_cards[i].draw(15, 30)
+                lista_cards[i].set_border(COLOR_BORDER, 5)
+            else:
+                lista_cards[i].fill()
+    else:
+        wait -= 1
+
+    for event in pygame.event.get():
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+            x, y = event.pos
+
+            for i in range(4):
+                if lista_cards[i].is_collide(x, y):
+                    if click == i:
+                        points += 1
+                        lista_cards[i].change_color(GREEN)
+                        lista_cards[i].set_border(COLOR_BORDER, 5)
+                    else:
+                        points -= 1
+                        lista_cards[i].change_color(RED)
+                        lista_cards[i].set_border(COLOR_BORDER, 5)
+                
+                    lista_cards[i].fill()
+
+
+    pygame.display.update()
+    reloj.tick(FPS)
+
+
+pygame.quit()
